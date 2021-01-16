e98a491bf922883a95ad9bd4244b83f79d52b700
vikrant121212 <77528721+vikrant121212@users.noreply.github.com>
2021-01-16 15:22:53 +0530

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -3,9 +3,13 @@
 
 /*
 * Write your query here
+select * from employees
+where  age between 30 and 50 AND income < 50000
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
 * Write your query here
+select avg(age) from employees
+WHERE age between 20 and 50
 */
