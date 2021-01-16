1c0f0ba7ce3333c9d46351a25045a6784cf4799d
vikrant121212 <77528721+vikrant121212@users.noreply.github.com>
2021-01-16 14:48:22 +0530

    Update questions.sql

--- SQL Deep Dive/3 Valued Logic Exercises/questions.sql
+++ SQL Deep Dive/3 Valued Logic Exercises/questions.sql
@@ -4,7 +4,7 @@
 * Table: customers
 * Question: adjust the following query to display the null values as "No Address"
 */
-SELECT address2 
+SELECT coalesce(address2, 'no address')
 FROM customers
 
 /*
@@ -15,7 +15,7 @@ FROM customers
 
 SELECT *
 FROM customers
-WHERE COALESCE(address2, null) IS NOT null;
+WHERE address IS NOT null;
 
 /*
 * DB: Store
@@ -24,4 +24,4 @@ WHERE COALESCE(address2, null) IS NOT null;
 */
 
 SELECT coalesce(lastName, 'Empty'), * from customers
-where (age = null);
+where (age is null);
